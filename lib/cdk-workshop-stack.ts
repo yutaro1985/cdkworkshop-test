cbc1d589f64815096e257e1c8be6287b45eabe7d
yutaro1985 <yutaro.suzuki@cloudys.dev>
2020-03-27 20:14:10 +0900

    test

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -7,12 +7,12 @@ export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
+    // const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
 
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
+    // const topic = new sns.Topic(this, 'CdkWorkshopTopic');
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
