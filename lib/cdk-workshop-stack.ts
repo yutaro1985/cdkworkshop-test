e16421891cb261288d297badd72cde6ee952a7fc
yutaro1985 <yutaro.suzuki@cloudys.dev>
2020-03-30 16:22:36 +0900

    Hello Lambdaまで

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,18 +1,14 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-
-    // const topic = new sns.Topic(this, 'CdkWorkshopTopic');
-
-    // topic.addSubscription(new subs.SqsSubscription(queue));
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler'
+    });
   }
 }
