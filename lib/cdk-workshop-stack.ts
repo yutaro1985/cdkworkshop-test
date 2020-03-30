fb2d7cb664d0425148febe8fa8c6605827da5114
yutaro1985 <yutaro.suzuki@cloudys.dev>
2020-03-30 20:05:54 +0900

    Writing Constructの途中。うまくいかない。

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,19 +1,24 @@
-import * as cdk from '@aws-cdk/core';
-import * as lambda from '@aws-cdk/aws-lambda';
-import * as apigw from '@aws-cdk/aws-apigateway';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as apigw from "@aws-cdk/aws-apigateway";
+import { HitCounter } from "./hitcounter";
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const hello = new lambda.Function(this, 'HelloHandler', {
+    const hello = new lambda.Function(this, "HelloHandler", {
       runtime: lambda.Runtime.NODEJS_10_X,
-      code: lambda.Code.fromAsset('lambda'),
-      handler: 'hello.handler'
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "hello.handler"
     });
 
-    new apigw.LambdaRestApi(this, 'Endpoint',{
-      handler: hello
-    })
+    const helloWithCounter = new HitCounter(this, "HelloHitCounter", {
+      downstream: hello
+    });
+
+    new apigw.LambdaRestApi(this, "Endpoint", {
+      handler: helloWithCounter.handler
+    });
   }
 }
